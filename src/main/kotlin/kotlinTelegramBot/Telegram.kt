fd73863cce1a9a9dca013aab1012ed6c6f962776
Evgeniy <Evgrazhev@yandex.ru>
2024-11-22 12:12:22 +0300

    KTB-14-regex

--- src/main/kotlin/kotlinTelegramBot/Telegram.kt
+++ src/main/kotlin/kotlinTelegramBot/Telegram.kt
@@ -6,7 +6,6 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 fun main(args: Array<String>) {
-
     val botToken = args[0]
     var updateId = 0
 
@@ -15,12 +14,20 @@ fun main(args: Array<String>) {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        println(updateIdString)
-        updateId = updateIdString.toInt() + 1
+        val updateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
+        val matchResultId: MatchResult? = updateIdRegex.find(updates)
+        val updateString = matchResultId?.groups?.get(1)?.value
+
+        if (updateString != null) {
+            updateId = updateString.toInt() + 1
+        }
+        println(updateId)
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+        println(text)
     }
 }
 
