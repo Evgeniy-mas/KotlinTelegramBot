1d390f941f394836c91e028e336d811e05fcb17a
Evgeniy <Evgrazhev@yandex.ru>
2024-11-22 12:15:18 +0300

    KTB-14-Regex

--- src/main/kotlin/kotlinTelegramBot/Telegram.kt
+++ src/main/kotlin/kotlinTelegramBot/Telegram.kt
@@ -6,20 +6,37 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 fun main(args: Array<String>) {
-
     val botToken = args[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    var updateId = 0
 
-    val builder: HttpClient.Builder = HttpClient.newBuilder()
-    val client: HttpClient = builder.build()
+    while (true) {
+        Thread.sleep(2000)
+        val updates: String = getUpdates(botToken, updateId)
+        println(updates)
 
-    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
-    val requestGetUpdates: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+        val updateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
+        val matchResultId: MatchResult? = updateIdRegex.find(updates)
+        val updateString = matchResultId?.groups?.get(1)?.value
 
+        if (updateString != null) {
+            updateId = updateString.toInt() + 1
+        }
+        println(updateId)
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+        println(text)
+    }
+}
+
+fun getUpdates(botToken: String, updateId: Int): String {
+
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-    val responseGetUpdates: HttpResponse<String> = client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString())
 
-    println(response.body())
-    println(responseGetUpdates.body())
+    return response.body()
 }
